7f129b19db69b029947d18595cdbcc25bbd336a5
NileshVS <50776400+NileshVS@users.noreply.github.com>
2019-08-30 19:07:17 +0530

    #learningExpress

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 const express = require('express');
 const app = express();
-console.log(app);
+
+let users = [{
+    id:1,
+    name:'Nilesh'
+},
+{
+    id:2,
+    name:'Kamal'
+},
+{
+    id:3,
+    name: 'Vasant'
+},
+{
+    id:4,
+    name:'Ashish'
+}
+];
+app.get('/api/users',(req,res) => {
+    res.send(users);
+});
+
+app.get('/api/users/:id', (req,res) => {
+    // let idPara = req.params.id;
+    // res.send(` The ID of the current user is ${idPara}.`);
+    let checkUserId = users.find(item => item.id === parseInt(req.params.id));
+    if(!checkUserId){
+        res.status(403).send('Invalid user ID')
+    }
+    else{
+        let currentUser=checkUserId;
+        res.send(currentUser);
+    }
+});
+
+app.listen(4000, () => console.log('Server is running at localhost:4000'));
